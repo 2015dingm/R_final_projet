30fd7421447c7069ccc5b0600dc1591e7b6e8d74
yang-au-carre <yang-au-carre@outlook.com>
2017-02-22 23:52:59 +0100

    杨阳

--- TP_final.R
+++ TP_final.R
@@ -6,15 +6,15 @@ dim(set)
 #renommer la nom de extra1 en ABS
 colnames(set)[5] <- "ABS"
 names(set)
-#colnames(set)[names(set)=="extra1"]="ABS"
 #Q2 
-#Repr??senter un scatter plot des quatre premi??res variables
+#Représenter un scatter plot des quatre premières variables
 library(corrplot)
 corrplot(cor(set[,1:4]), method="circle")
 #boxplot the two relations
 par(mfrow=c(2,1))
 boxplot(set$price~set$ABS,main="price/ABS",xlab = "ABS",ylab = "Price")
 boxplot(set$price~set$extra2,main = "price/TIA",xlab = "toit de ouvrant",ylab = "Price")
+
 #signification:
 # according to the first boxplot, the presence of ABS can increase the price
 # according to the second boxplot, the presence of "toit d'ouvrant" has no influence on the price
@@ -26,13 +26,13 @@ boxplot(set$price~set$extra2,main = "price/TIA",xlab = "toit de ouvrant",ylab = 
 model1=lm(set$price~set$ABS,data=set)
 summary(model1)
 # interpretation: -----------
-#Q4 ??tude du prix en fonction du kilom??trage:
+#Q4 Étude du prix en fonction du kilométrage:
 #a)
 model2 = lm(price~km,data = set)
 summary(model2) 
 # interpretation: -----------
 # it seems the longer the kM is, the smaller the price is 
-#b)??????????????????????????????50 000km??????????????????????????????bootstrap????????????????????????????
+#b)我不太确定怎么做，把50 000km的数据挑出来，然后用bootstrap?是不是我想麻烦了？
 confint(model2, "set$km")
 IC1=predict(model2, newdata = data.frame(km = 50), interval = "confidence")
 max(IC1)-min(IC1)#0.6604941
@@ -41,7 +41,7 @@ max(IC2)-min(IC2)#0.3075579
 dev.off()
 #c) this is almost a linear function: kop1 = 0.02243*km - 3.02203
 plot(set$km,set$kop1,main = "linear")
-lm= lm(set$kop1~set$km,data = set)
+lm= lm(kop1~km,data = set)
 #Coefficients:
 #  (Intercept)       set$km  
 #-3.02203      0.02243  
@@ -50,8 +50,9 @@ qqnorm(rstudent(lm),
        main=c("pval=", round(shapiro.test(rstudent(lm))$p.value,2)))
 qqline(rstudent(lm))
 max(rstudent(lm))
-which.max(rstudent(lm)) # plus grand r?sidu
+which.max(rstudent(lm)) # plus grand residu
 plot(lm)
+
 
 #reduis = set$kop1-(lm$coefficients[1]+lm$coefficients[2]*set$km)
 #m = mean(reduis)
@@ -62,40 +63,52 @@ plot(lm)
 #qqline(k)
 
 #d)
-km = set$km
-model3 = lm(set$price~km+I(km^2)+I(km^3),data = set)
+model3 = lm(price~km+I(km^2)+I(km^3),data = set)
 summary(model3)
 # in fact, it is still a polynomial regression
-#e) non, it's different.??????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
-#??????????????????????????????????????????????????????????????????
-model3_1 = lm(set$price~I(km^3) + I(km^2)+ km,data = set)
-summary(model3_1)
-anova(model3_1)
+#e) non, it's different.如果因子不止一个，且别是非平衡设计，或者存在协变量，表达式中的顺序会对结果造成影响。
+#样本大小越不平衡，效应项的顺序对结果影响越大
+model3_b = lm(price~I(km^3) + I(km^2)+ km,data = set)
+summary(model3_b)
+anova(model3_b)
 anova(model3)
-anova(model3,model3_1)
+anova(model3,model3_b)
 #f) you need to install this package, because it's not contained initially by R
 library(car)
-result = vif(lm(set$price~km+I(km^2)+I(km^3),data = set))
-#comment: -------
+result = vif(lm(price~km+I(km^2)+I(km^3),data = set))
+#km  I(km^2)  I(km^3) 
+#165.3163 787.3501 262.5217 
+
 #calcul direct:1/(1-Rj^2) 
 mkm=lm(km~I(km^2)+I(km^3), data = set)
-(vif_km=1/(1-summary(mkm)$r.squared^2))
-#82.90889
+(vif_km=1/(1-summary(mkm)$r.squared))
+#165.3163
 mkm2=lm(I(km^2)~I(km^3)+km, data = set)
-(vif_km2=1/(1-(summary(mkm2)$r.squared)^2))
-#393.9252
+(vif_km2=1/(1-(summary(mkm2)$r.squared)))
+#787.3501
 mkm3=lm(I(km^3)~I(km^2)+km, data = set)
-(vif_km3=1/(1-(summary(mkm3)$r.squared)^2))
-#131.5113
+(vif_km3=1/(1-(summary(mkm3)$r.squared)))
+#262.5217 
+#comment: -------
 #g)
-lr2 = lm(set$kop2~km+I(km^2),data = set)
+#The VIF equals 1 when the vector Xj is orthogonal to each column of the design matrix for the regression of Xj on the other covariates.
+model4 = lm(price~kop1+kop2+kop3,data = set)
+(vif_model4 = vif(model4))
+mean(set$kop1)
+sd(set$kop1)
+mean(set$kop2) 
+sd(set$kop2) 
+mean(set$kop3)
+sd(set$kop3) 
+
+lr2 = lm(kop2~km+I(km^2),data = set)
 summary(lr2)
 #Coefficients:
 #  Estimate Std. Error    t value Pr(>|t|)    
 #(Intercept)  6.288e+00  1.114e-08  564703221   <2e-16 ***
 #  km          -1.022e-01  1.653e-10 -618221041   <2e-16 ***
 #  I(km^2)      3.714e-04  5.868e-13  632936828   <2e-16 ***
-lr3 = lm(set$kop3~km+I(km^2)+I(km^3),data = set)
+lr3 = lm(kop3~km+I(km^2)+I(km^3),data = set)
 summary(lr3)
 #Coefficients:
 #  Estimate Std. Error    t value Pr(>|t|)    
@@ -103,10 +116,11 @@ summary(lr3)
 #  km           2.687e-01  4.370e-10  614916710   <2e-16 ***
 #  I(km^2)     -2.203e-03  3.386e-12 -650691516   <2e-16 ***
 #  I(km^3)      5.406e-06  8.192e-15  659873143   <2e-16 ***
-#Q5 methode1
-#1)
+
+#Q5 
+#1)methode1
 library(MASS)
-lmf = stepAIC(lm(set$price~.,data = set))
+lmf = stepAIC(lm(price~.,data = set))
 #set$price ~ age + km + kop2 + ageop2
 #Df Sum of Sq     RSS     AIC
 #<none>                 91.391 -98.764
@@ -114,6 +128,14 @@ lmf = stepAIC(lm(set$price~.,data = set))
 #- ageop2  1     5.858  97.248 -90.079
 #- km      1    28.140 119.531 -54.594
 #- age     1    71.985 163.376  -0.848
+#methode2  stepwise
+lmf_b=lm(price~km,data=set); 
+summary(lmf_b)
+#mod=paste("~",paste("x",1:10,sep="",collapse="+"))
+mod = "~price + age+km+TIA+ABS+extra2+kop1+kop2+kop3+ageop1+ageop2+ageop3"
+st2=stepAIC(lmf_b,scope=list(lower=~1,upper=mod),
+            direction="both")
+st2$anova
 #2)
 summary(lmf)
 qqnorm(rstudent(lmf),
